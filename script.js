34dfdea8ba5fae0c49295aa05e9eea324ebe0738
Shameelamuskan <143951492+Shameelamuskan@users.noreply.github.com>
2023-09-29 21:55:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+<script>
+let outputScreen = document.getElementById("output-screen");
+function display(num){
+    outputScreen.value += num;
+}
+function Calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid")
+    }
+}
+function Clear(){
+    outputScreen.value = "";
+}
+
+
+</script>
